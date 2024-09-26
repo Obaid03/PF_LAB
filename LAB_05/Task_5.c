228c570f3608bba6ae75b2f2a3adc3d1a46e10ee
OBAID <muhammadobaid103@gmail.com>
2024-09-26 22:19:35 +0500

    Update Task_5.c

--- LAB_05/Task_5.c
+++ LAB_05/Task_5.c
@@ -5,7 +5,7 @@ Assume that the grade point average is within the range 0.0 through 4.0.*/
 #include<stdio.h>
 int main(){
     float gpa;
-    printf("enter your GPA");
+    printf("enter your GPA\n");
     scanf("%f",&gpa);
 
     (gpa>=0.00 && gpa<1.00)?printf("Failed semester"):(gpa>=1.00 && gpa<1.99)?printf("On probation for next semester"):(gpa>=2.00 && gpa<3.00)?printf("-"):(gpa>=3.00 && gpa<3.50)?printf("Dean's List For semester"):(gpa>=3.50 && gpa<=4.00)?printf("Highest Honor For Semester"):printf("Invalid GPA Entered");
