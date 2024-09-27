e784ab1036ad10a0c6e3772457d3074ffb8999f1
OBAID <muhammadobaid103@gmail.com>
2024-09-27 23:58:00 +0500

    Update Task_4.c

---
+++ LAB_05/Task_4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int ph;
+	printf("Enter PH: ");
+	scanf("%d", &ph);
+	
+	if (ph>7)
+  {
+		if (ph<12)
+        {printf("Alkaline");}
+		else
+        {printf(" Very Alkaline");}
+	}
+	else
+  { 
+    if (ph==7){
+			  printf("Neutral");}
+		else if(ph>2)
+        {printf("Acidic");}
+		else
+        {printf("Very Acidic");}
+	}
+return 0;
+}
