5e5569ea4616b1d3f145c3f6e88d58defaa263d7
OBAID <muhammadobaid103@gmail.com>
2024-10-10 07:24:43 +0500

    Update Task4.c

--- LAB_06/Task4.c
+++ LAB_06/Task4.c
@@ -1,1 +1,19 @@
-
+#include<stdio.h>
+int main ()
+{
+    int n,ld,sum=0,i;
+    printf("Enter Number\t");
+    scanf("%d",&n);
+    i=n;
+    while(i!=0)
+    {
+        ld=i%10;
+        sum=sum+(ld*ld*ld);
+        i=i/10;   
+    }
+    if (sum==n)
+    printf("The number is an armstrong");
+    else
+    printf("The number is not an armstrong"); 
+    return 0;
+}
