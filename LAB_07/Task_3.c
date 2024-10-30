4208cbc5745d5d9d59c20634831e665f5d1e68da
OBAID <muhammadobaid103@gmail.com>
2024-10-31 00:05:02 +0500

    Update Task_3.c

--- LAB_07/Task_3.c
+++ LAB_07/Task_3.c
@@ -1,1 +1,30 @@
-q
+/* Write a program that calculates the length of a string using a pointer. Do not use the
+built-in strlen() or sizeof() function. After length calculation reverse the string using
+the same pointers */
+
+#include <stdio.h>
+int main() {
+    char arr[100];
+    int length = 0;
+    char *ptr = arr;
+
+    printf("Enter a string (without spaces): ");
+    scanf("%s", arr);
+    while (*ptr != '\0') {
+        printf("%c", *ptr);
+        length++;           
+        ptr++;             
+    }
+    
+    printf("\n");
+    ptr--;
+    char n[length];
+    for (int i = 0; i < length; i++)
+    {
+        n[i] = *ptr;
+        ptr--;
+    }
+    printf("%s", n);
+    printf("\nLength of the string: %d\n", length);
+    return 0;
+}
