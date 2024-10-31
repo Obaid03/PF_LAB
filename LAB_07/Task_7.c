18073c981f853898139d22d14850fa5ae55d9071
OBAID <muhammadobaid103@gmail.com>
2024-10-31 15:17:54 +0500

    Create Task_7.c

---
+++ LAB_07/Task_7.c
@@ -0,0 +1,8 @@
+/* You are required to design a calculator utilizing an array. Take a string from the
+user i.e. 20+10-30 and store it in an array. Traverse through the array, if there are
+values stored them in some variable and if an operation comes perform the
+necessary operation and if a null character comes display the result. If the null
+character comes after the operator, the program should print an invalid
+expression. (Note: Only do this for + and - operator) */
+
+
