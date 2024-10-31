15341f1b68e4804bc9a4aaee7f4ff491ce9a53a6
OBAID <muhammadobaid103@gmail.com>
2024-10-31 13:40:27 +0500

    Update Task_4.c

--- LAB_07/Task_4.c
+++ LAB_07/Task_4.c
@@ -1,12 +1,14 @@
 /* Write a program which identifies if a given string is a palindrome using arrays. */
 #include <stdio.h>
+#include <string.h>
 int main(){
-    char word[3];
-    int i,p=0, size= sizeof(word)/sizeof(word[0]);
+    char word[50];
+    
     printf("Enter the Word\t");
     scanf("%s",word);
-    for(i=0;i<3;i++){
-        if (word[i]==word[3-1]){
+    int i,p=0, size = strlen(word);
+    for(i=0;i<(size/2);i++){
+        if (word[i]==word[size-i-1]){
             p=1;
             continue;
         }
