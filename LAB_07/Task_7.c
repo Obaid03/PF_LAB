09d7cb1d999043ca5e7b67a2561ab95e8a0e2cbe
OBAID <muhammadobaid103@gmail.com>
2024-11-02 00:31:51 +0500

    Update Task_7.c

--- LAB_07/Task_7.c
+++ LAB_07/Task_7.c
@@ -5,4 +5,48 @@ necessary operation and if a null character comes display the result. If the nul
 character comes after the operator, the program should print an invalid
 expression. (Note: Only do this for + and - operator) */
 
+#include<stdio.h>
+
+int main (){
+    int result=0,num=0,i,size;
+	char equation[100];
+	char operator= '+';
+	
+	printf("It is the simple calculator that performs Addition and Subtraction only");
+	printf("\nEnter the equation : ");
+	scanf("%s",&equation[0]);
+	size= sizeof(equation)/sizeof(equation[0]);
+    // if(equation[size]<'0'||equation[size]>'9'){
+    //     printf("invalid input.\t syntax error");
+    // }
+
+
+	for(i=0;i<size;i++){
+		if(equation[i]>='0'&&equation[i]<='9')
+		num=num*10+(equation[i]-48);
+		else if (equation[i]=='+'||equation[i]=='-')
+		   { if(i==0){
+		        result=num;}
+		    else {
+                result=result+num;}
+		        num=0;
+		   if(equation[i]=='-'){
+		   	    result=result-num;
+		   	    operator='-';
+		    }
+		}
+	} 
+    if (operator=='+')
+	    result=result+num;
+    else if(operator=='-')
+	    result=result-num;
+	
+    printf("%d",result);
+    return 0;
+}
+	
+          
+
+
+
 
