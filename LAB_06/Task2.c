847fe49f30c3a58219d5f49891abcae70c324533
OBAID <muhammadobaid103@gmail.com>
2024-11-02 10:34:30 +0500

    Update Task2.c

--- LAB_06/Task2.c
+++ LAB_06/Task2.c
@@ -5,6 +5,10 @@ int main()
     int i,n;
     printf("Enter the Number\t");
     scanf("%d",&n);
+    if(n==1||n==0){
+        printf("It is not a prime number");
+        return 0;
+    }
     for (i=2;i<n;i++)
     {
         if (n%i==0)
