63d9b30bd9264896a38a9208684dd7544996f8b4
OBAID <muhammadobaid103@gmail.com>
2024-11-02 11:18:47 +0500

    Update Task3.c

--- LAB_06/Task3.c
+++ LAB_06/Task3.c
@@ -1,29 +1,44 @@
 #include <stdio.h>
-int main ()
-{
-    int N,i,sum=0,f=0,s=1;
-    printf("Enter the Number of Terms\n");
-    scanf("%d",&N);
-   for (i=0;i<N-3;i++)
-    {
-        switch (N){
-        	case 1:
-            	printf("0");
-            	break;
-        default:
-        	switch (i) {
-        		case 0:
-        			printf("%d %d",f,s);
-        			sum=f+s;
-            		printf(" %d ",sum);
-           	 		f=s;
-            		s=sum;
-				default :
-					sum=f+s;
-            		printf(" %d ",sum);
-            		f=s;
-            		s=sum;
-            	}
-        }}
-    return 0;
+
+int main() {
+    int i, n, sum = 0, f = 0, s = 1, prime = 1;;
+
+    printf("Enter the Number: ");
+    scanf("%d", &n);
+
+    if (n == 1 || n == 0) {
+        printf("%d is not a prime number.\n", n);
+    } else {
+        for (i = 2; i<n; i++) {
+            if (n % i == 0) {
+                prime = 0; 
+                break;
+            }
+        }
+
+        if (prime) {
+            printf("%d is a prime number.\n", n);
+            printf("Fibonacci series up to %d:\n", n);
+
+            if (n >= 0) {
+                printf("%d ", f);
+            }
+            if (n >= 1) {
+                printf("%d ", s);
+            }
+            for (i = 2;sum < n-1 ; i++) {
+                sum = f + s;
+                if (sum>n){
+                    break;}
+                printf("%d ", sum);
+                f = s;
+                s = sum;
+            }
+            printf("\n");
+        } else {
+            printf("%d is not a prime number.\n", n);
+        }
     }
+
+    return 0;
+}
